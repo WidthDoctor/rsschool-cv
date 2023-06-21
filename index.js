955f2daaf77bd043c4863648dc12cdf8978e13f7
WidthDoctor <elskel1812@gmail.com>
2023-06-22 01:53:09 +0200

    feat: add section to script + add secret (Чт, 22 июн 2023 01:53:09)01~

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ burger.addEventListener('click',function(){
 
 
 function burgerOFF(){
+  allSectionHide();
   HEADER.classList.remove('little');
   burger.classList.add('hide');
   setTimeout(() => {
@@ -45,6 +46,94 @@ function menuOFF() {
   }, 500);
 }
 
+const ABOUT = document.getElementById('about');
+const skillsElement = document.getElementById('skills');
+const educationElement = document.getElementById('education');
+const experienceElement = document.getElementById('experience');
+const codeElement = document.getElementById('code');
+const languagesElement = document.getElementById('languages');
+
+const sectionAbout = document.querySelector('.about');
+const sectionSkills = document.querySelector('.skills');
+const sectionEducation = document.querySelector('.education');
+const sectionExperience = document.querySelector('.experience');
+const sectionCode = document.querySelector('.code');
+const sectionLanguages = document.querySelector('.languages');
+
+function showSection(section) {
+  section.classList.remove('none');
+}
+
+ABOUT.addEventListener('click', function() {
+  showSection(sectionAbout);
+});
+
+skillsElement.addEventListener('click', function() {
+  showSection(sectionSkills);
+});
+
+educationElement.addEventListener('click', function() {
+  showSection(sectionEducation);
+});
+
+experienceElement.addEventListener('click', function() {
+  showSection(sectionExperience);
+});
+
+codeElement.addEventListener('click', function() {
+  showSection(sectionCode);
+});
+
+languagesElement.addEventListener('click', function() {
+  showSection(sectionLanguages);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const repeatCounts = {
+    'Vs code': 4,
+    'GIT': 3,
+    'HTML': 4,
+    'CSS': 4,
+    'JAVA SCRIPT': 3,
+    'NODE.JS':2,
+    'TYPESCRIPT': 1,
+    'FIGMA': 2,
+    'ADOBE PHOTOSHOP': 2,
+    'DEEPL':4
+  };
+
+  const hamsters = document.querySelectorAll('.hamster_wrapper');
+
+  hamsters.forEach((hamster) => {
+    const skillElement = hamster.closest('.skill');
+    const elementText = skillElement.querySelector('span').textContent;
+
+    if (repeatCounts.hasOwnProperty(elementText)) {
+      const repeatCount = repeatCounts[elementText];
+      const imgElement = hamster.querySelector('img');
+
+      for (let i = 0; i < repeatCount; i++) {
+        const imgClone = imgElement.cloneNode(true);
+        hamster.appendChild(imgClone);
+      }
+    }
+  });
+});
 
 
+
+
+function allSectionHide(){
+  sectionAbout.classList.add('none');
+  sectionSkills.classList.add('none');
+}
+
+const skillElements = document.querySelectorAll('.skill');
+
+
+skillElements.forEach(function(skillElement) {
+  skillElement.addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=y2vPiGMlQl0&ab_channel=OFFICEGUY';
+  });
+});
 
